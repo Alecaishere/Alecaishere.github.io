1edaac9975a7d29933b22cac0ccb99823dd030b3
Alecaishere <alejandro.delgado.morillo.01@gmail.com>
2025-05-26 22:20:14 +0200

    Trying things

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type React from "react"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Alejandro Delgado Morillo - Presentation Card",
+  description: "CV de Alejandro DM - Técnico de mantenimiento de equipos informáticos",
+  icons: {
+    icon: "/images/ALE-fondo.png",
+    apple: "/images/ALE-fondo.png",
+  },
+    generator: 'v0.dev'
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="es">
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
